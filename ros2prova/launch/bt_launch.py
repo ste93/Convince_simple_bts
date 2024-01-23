31f0470995207ef20a397fce4bbfd08b4623e2f8
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-01-23 17:53:14 +0100

    removed debug prints

--- ros2prova/launch/bt_launch.py
+++ ros2prova/launch/bt_launch.py
@@ -6,30 +6,27 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='alarm_battery_low_skill',
-            executable='alarm_battery_low_skill',
+            executable='alarm_battery_low_skill'
             output='screen',
-            arguments=['--ros-args', '--log-level', 'debug']
+            # arguments=['--ros-args', '--log-level', 'debug']
         ),
         Node(
             package='battery_drainer_skill',
             executable='battery_drainer_skill',
-            output='screen',
-            arguments=['--ros-args', '--log-level', 'debug']
+            output='screen'
         ),
         Node(
             package='battery_level_skill',
-            executable='battery_level_skill',
-            arguments=['--ros-args', '--log-level', 'debug']
+            executable='battery_level_skill'
         ),
         Node(
             package='battery_drainer_component',
-            executable='battery_drainer_component',
-            arguments=['--ros-args', '--log-level', 'debug']
+            executable='battery_drainer_component'
         ),
         Node(
             package='bt_executable',
             executable='bt_executable',
-            arguments=[ './BT/simple_bt.xml','--ros-args', '--log-level', 'debug']
+            arguments=[ './BT/simple_bt.xml']
         )
     ])
 
